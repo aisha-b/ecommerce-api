03680b2ead56346f297269ca88d547a29a8c7d4c
AishaBaigtu <aishabaigtu@gmail.com>
2022-01-20 02:05:14 +0800

    Add cors middleware header

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
