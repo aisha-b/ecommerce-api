5c9aa60c75ae699845d9d94ab8fee41335f7bfa3
AishaBaigtu <aishabaigtu@gmail.com>
2022-01-20 12:57:08 +0800

    Add cors header

--- index.js
+++ index.js
@@ -26,9 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-react-website-km6g724vt-aishabaigtu.vercel.app');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    next();
 });
 
 app.use("/api/users", userRoute);
